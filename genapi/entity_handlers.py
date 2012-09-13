743553e41f960e55db2da6e07cdc04ef9dc6f6c4
Hans-Gunther Schmidt <hans@otype.de>
2012-09-13 18:46:09 +0200

    streamlining error status codes

--- genapi/entity_handlers.py
+++ genapi/entity_handlers.py
@@ -19,7 +19,11 @@ import uuid
 import time
 from analytics import send_analytics_data
 from base_handlers import BaseHandler
-from entity_handlers_helpers import get_single_object, search, fetch_all, illegal_attributes_exist, filter_out_timestamps, pop_field
+from entity_handlers_helpers import get_single_object
+from entity_handlers_helpers import search
+from entity_handlers_helpers import fetch_all
+from entity_handlers_helpers import illegal_attributes_exist
+from entity_handlers_helpers import filter_out_timestamps
 from response import Response
 
 
@@ -153,13 +157,13 @@ class SimpleEntityHandler(BaseHandler):
         db_object = self.bucket.get(object_id).get_data()
 
         if db_object is None:
-            self.write_error(410, message='Cannot update object: object with given id does not exist!')
+            self.write_error(404, message='Cannot update object: object with given id does not exist!')
             return
 
         try:
             obj_to_store = json.loads(unicode(self.request.body, 'latin-1'))
             if obj_to_store is None:
-                self.write_error(304, 'Updating object with id: {} not possible.'.format(object_id))
+                self.write_error(400, 'Updating object with id: {} not possible.'.format(object_id))
                 return
 
             # Filter out the time stamps if they exist
@@ -198,7 +202,7 @@ class SimpleEntityHandler(BaseHandler):
 
         db_obj = self.bucket.get(object_id)
         if db_obj.get_data() is None:
-            self.write_error(410, 'Object with id: {} does not exist.'.format(object_id))
+            self.write_error(404, 'Object with id: {} does not exist.'.format(object_id))
             return
 
         result = db_obj.delete()
@@ -207,5 +211,5 @@ class SimpleEntityHandler(BaseHandler):
             self.set_status(200)
             self.write(Response(status_code=200, status_message='OK', result={"deleted": object_id}).get_data())
         else:
-            self.write_error(410, 'Could not delete object with id: {}'.format(object_id))
+            self.write_error(404, 'Could not delete object with id: {}'.format(object_id))
 
