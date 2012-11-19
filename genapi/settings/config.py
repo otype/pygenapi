8d3901d409ce2ad6dc99736bad1423ad580ae643
Hans-Gunther Schmidt <hans@otype.de>
2012-11-19 23:51:30 +0100

    bumping up version to 0.4.1

--- genapi/settings/config.py
+++ genapi/settings/config.py
@@ -10,7 +10,7 @@
 # Application details
 APP_DETAILS = {
     'name': 'PyGenAPI',
-    'version': '0.4',
+    'version': '0.4.1',
     'company': 'apitrary',
     'support': 'http://apitrary.com/support',
     'contact': 'support@apitrary.com',
